74a25c04519e60af72cfed897ef1d22c4517f573
fampatta <fampatta1234@gmail.com>
2020-01-28 20:36:12 +0700

    tyu

---
+++ untitled0.py
@@ -0,0 +1,14 @@
+L = [] #line 1
+f = open("exam.txt") #line 2
+L = [a.strip().split(',') for a in f] #line 4
+print(L) #line 5
+f.close() #line 6
+
+
+
+#data1  =  [-3,'-1',1,'3',5,'7',9]
+#data2  =  [2,2.5,3,3.5,4,4.5,5]
+#
+#print(data1[-1::-2])
+
+
